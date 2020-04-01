7db8c170b36280dd96e284f6579f720d75e83690
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-01 16:31:04 +0530

    [Shivam] Accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.

--- program.py
+++ program.py
@@ -4,7 +4,18 @@ class programWeek1:
         self.fname = input("Enter your First name : ")   # take input from user
         self.lname = input("Enter your Last name : ")    # take input from user
         print(self.fname[::-1],self.lname[::-1])         # reverse first and last name 
+    
+    def list_tuple(self):
+        self.number = input("Input some comma seprated numbers : ")   # take input from user
+        self.list = self.number.split(",")   # split() method splits a string into a list.
+        self.tuple = tuple(self.list)        # convert list to tuple using tuple() function.
+        print('List : ',self.list)
+        print('Tuple : ',self.tuple)
 
+def main():
+    obj = programWeek1()   # create object of class
+    obj.reverse_name()     # calling methods(function) in class
+    obj.list_tuple()
 
-obj = programWeek1()   # create object of class
-obj.reverse_name()     # calling methods(function) in class
+if __name__ == "__main__":
+    main()
