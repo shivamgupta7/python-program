ecf4441205c7b0bb2a0628189a24a7dfeaaa079c
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-04 11:52:21 +0530

    [Shivam] Reverse the order of the item in the array.

--- arrayProgram.py
+++ arrayProgram.py
@@ -12,9 +12,18 @@ class programArray:
             print(ele)
         return array_num[0], array_num[1], array_num[2], array_num[3], array_num[4]
 
+    def reverse_array(self, lst):
+        '''
+        Reverse the order of the items in the array.
+        '''
+        self.array_num = array('i', lst)
+        self.array_num.reverse()
+        return self.array_num
+
 def main():
     obj = programArray()
     print(obj.create_int_array([3,5,2,8,7]))
+    print(obj.reverse_array([3,5,2,8,7]))
 
 if __name__ == "__main__":
     main()
