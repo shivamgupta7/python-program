8c68dc42a8f1013c8779e9503ff0ba174c49613f
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-04 12:34:45 +0530

    [Shivam] Remove the first occurrence of a specified element from an array.

--- arrayProgram.py
+++ arrayProgram.py
@@ -29,11 +29,21 @@ class programArray:
         self.occurr = self.array_num.count(self.element)
         return self.occurr
 
+    def remove_occurrence(self, lst):
+        '''
+        Remove the first occurrence of a specified element from an array.
+        '''
+        self.array_num = array('i', lst)
+        self.element = int(input("Enter the element for remove the first occurrences : "))
+        self.array_num.remove(self.element)
+        return self.array_num
+
 def main():
     obj = programArray()
     print(obj.create_int_array([3,5,2,8,7]))
     print(obj.reverse_array([3,5,2,8,7]))
-    print(obj.count_occurence([1, 3, 6, 4, 3, 7, 3, 9, 3]))
+    print(obj.count_occurrence([1, 3, 6, 4, 3, 7, 3, 9, 3]))
+    print(obj.remove_occurrence([1, 3, 6, 4, 3, 7, 3, 9, 3]))
 
 if __name__ == "__main__":
     main()
