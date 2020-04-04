a1bc714ed831a6934152bf9b9a94e6738d6eda5c
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-04 12:25:51 +0530

    [Shivam] Get the number of occurrences of a specified element in an array.

--- arrayProgram.py
+++ arrayProgram.py
@@ -20,10 +20,20 @@ class programArray:
         self.array_num.reverse()
         return self.array_num
 
+    def count_occurrence(self, lst):
+        '''
+        Get the number of occurrences of a specified element in an array.
+        '''
+        self.array_num = array('i', lst)
+        self.element = int(input("Enter the element for find number of occurrences : "))
+        self.occurr = self.array_num.count(self.element)
+        return self.occurr
+
 def main():
     obj = programArray()
     print(obj.create_int_array([3,5,2,8,7]))
     print(obj.reverse_array([3,5,2,8,7]))
+    print(obj.count_occurence([1, 3, 6, 4, 3, 7, 3, 9, 3]))
 
 if __name__ == "__main__":
     main()
