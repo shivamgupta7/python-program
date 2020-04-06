e98f72c99f915375b5de3f5471590f704e0c63d1
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-07 00:15:48 +0530

    [Shivam] Remove items from set.

--- setProgram.py
+++ setProgram.py
@@ -22,8 +22,19 @@ class programSet:
         mySet.update(self.members)
         return mySet
 
+    def remove_items(self, mySet):
+        '''
+        Remove items from set
+        '''
+        print("My set is: ", mySet)
+        times = int(input('How many items you want to remove? : '))
+        for time in range(times):
+            item = int(input('Enter {0} item which one you want to delete : '.format(time+1)))
+            mySet.discard(item)        # If the item to remove does not exist, discard() will NOT raise an error.
+        return mySet
+
 def menu():
-    print('1.Create a set\n2.Iteration over sets\n3.Add new members')
+    print('1.Create a set\n2.Iteration over sets\n3.Add new members\n4.Remove items from set')
 
 def main():
     menu()
@@ -35,6 +46,8 @@ def main():
         obj.iterate_set({2,7,6,8,3,5})
     elif choice == 3:
         print(obj.addNewMembers({2,7,6,8,3,5}))
+    elif choice == 4:
+        print(obj.remove_items({2,5,3,9,7,15,1,8}))
     else:
         print('Please choose correct choice.')
     
