f6c67809c4b1034f45df6e286b30b63818cac06a
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-06 22:42:56 +0530

    [Shivam] Iteration over sets.

--- setProgram.py
+++ setProgram.py
@@ -7,8 +7,15 @@ class programSet:
         self.new_set = set(data)
         return self.new_set
 
+    def iterate_set(self,set_data):
+        '''
+        Iteration over sets
+        '''
+        for item in set_data:
+            print(item)
+
 def menu():
-    print('1.Create a set')
+    print('1.Create a set\n2.Iteration over sets')
 
 def main():
     menu()
@@ -16,6 +23,8 @@ def main():
     obj = programSet()
     if choice == 1:
         print(obj.create_set([1,2,3,4]))
+    elif choice == 2:
+        obj.iterate_set({2,7,6,8,3,5})
     else:
         print('Please choose correct choice.')
     
