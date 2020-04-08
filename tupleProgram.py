bd3a77dd292837e46ed10ef100555867480bd708
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-08 18:08:20 +0530

    [Shivam] Create a tuple with different data types.

--- tupleProgram.py
+++ tupleProgram.py
@@ -7,11 +7,26 @@ class programTuple:
         myTuple = (int(item) for item in input("Enter the items(seprated by space) for create tuple: ").split())
         return tuple(myTuple)
 
+    def tulpleDifferentDataType(self, data):
+        '''
+        Create a tuple with different data types
+        '''
+        myTuple = tuple(data)
+        return myTuple
+
+    def unpackTuple(self):
+        '''
+        Unpack a tuple in several variables
+        '''
+        myTuple = (int(item) for item in input("Enter the 3 integer(seprated by space) for create tuple: ").split())
+        item1, item2, item3 = myTuple
+        return item1, item2, item3
+
 def menu():
     '''
     Menu of programs
     '''
-    print('''1.Create a tuple''')
+    print('''1.Create a tuple\n2.Create a tuple with different data types\n3.Unpack a tuple in several variables''')
 
 def switchToFunction(case, obj):
     '''
@@ -19,6 +34,8 @@ def switchToFunction(case, obj):
     '''
     switcher = {
         1 : lambda: obj.createTulple(),
+        2 : lambda: obj.tulpleDifferentDataType(([3,2], False, 3.2, 'Shivam')),
+        3 : lambda: obj.unpackTuple(),
         }
     func = switcher.get(case, lambda: 'Invalid choice please select correct options.')
     print(func())
