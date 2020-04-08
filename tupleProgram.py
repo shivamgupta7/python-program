97840fdd70856079eb535d034f4cde912ddd1d26
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-08 19:45:19 +0530

    [Shivam] Create the colon of a tuple.

--- tupleProgram.py
+++ tupleProgram.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class programTuple:
 
     def createTulple(self):
@@ -22,11 +24,28 @@ class programTuple:
         item1, item2, item3 = myTuple
         return item1, item2, item3
 
+    def colonOfTuple(self):
+        '''
+        Create the colon of a tuple
+        '''
+        #create a tuple
+        myTuple = ("HELLO", 5, [], True) 
+        print("Original tuple",myTuple)
+        #make a copy of a tuple using deepcopy() function
+        tuple_colon = deepcopy(myTuple)
+        tuple_colon[2].append(50)
+        print("Copy of original tuple with modified :",tuple_colon)
+
 def menu():
     '''
     Menu of programs
     '''
-    print('''1.Create a tuple\n2.Create a tuple with different data types\n3.Unpack a tuple in several variables''')
+    print('''
+    1.Create a tuple
+    2.Create a tuple with different data types
+    3.Unpack a tuple in several variables
+    4.Create the colon of a tuple
+    ''')
 
 def switchToFunction(case, obj):
     '''
@@ -36,6 +55,7 @@ def switchToFunction(case, obj):
         1 : lambda: obj.createTulple(),
         2 : lambda: obj.tulpleDifferentDataType(([3,2], False, 3.2, 'Shivam')),
         3 : lambda: obj.unpackTuple(),
+        4 : lambda: obj.colonOfTuple(),
         }
     func = switcher.get(case, lambda: 'Invalid choice please select correct options.')
     print(func())
