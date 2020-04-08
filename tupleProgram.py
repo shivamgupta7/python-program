fa9b9e4af867e4a2486689d6609607603959b99a
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-08 22:36:25 +0530

    [Shivam] Find the repeated items of a tuple.

--- tupleProgram.py
+++ tupleProgram.py
@@ -36,6 +36,31 @@ class programTuple:
         tuple_colon[2].append(50)
         print("Copy of original tuple with modified :",tuple_colon)
 
+    def repeatedItems(self):
+        '''
+        Find the repeated items of a tuple
+        '''
+        myTuple = tuple(int(item) for item in input("Enter the items(seprated by space) for find repeated items: ").split())
+        repeat_ele = [] # create a tuple for storing repeated items
+        flag = False
+        for ele in myTuple:
+            # To check if Duplicate exist 
+            if ele in repeat_ele:
+                flag = True
+                continue
+            else:
+                countRepeatedEle = 0
+                for item in myTuple: 
+                    if ele == item:
+                        countRepeatedEle += 1
+                # To print count if Duplicate of element exist
+                if(countRepeatedEle > 1):  
+                    print('{0} repeated {1} times.'.format(ele, countRepeatedEle)) 
+                    repeat_ele.append(ele)
+        if flag == False: 
+            print("No Duplicates")
+        return repeat_ele
+
 def menu():
     '''
     Menu of programs
@@ -45,6 +70,7 @@ def menu():
     2.Create a tuple with different data types
     3.Unpack a tuple in several variables
     4.Create the colon of a tuple
+    5.Find the repeated items of a tuple
     ''')
 
 def switchToFunction(case, obj):
@@ -56,6 +82,7 @@ def switchToFunction(case, obj):
         2 : lambda: obj.tulpleDifferentDataType(([3,2], False, 3.2, 'Shivam')),
         3 : lambda: obj.unpackTuple(),
         4 : lambda: obj.colonOfTuple(),
+        5 : lambda: obj.repeatedItems(),
         }
     func = switcher.get(case, lambda: 'Invalid choice please select correct options.')
     print(func())
