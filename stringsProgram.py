6906f12ae77d19714466a0eeb02a6c8ab5eeda9b
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-09 11:13:15 +0530

    [Shivam] Add 'ing' at the end of given string. if the given string already end with 'ing' then add 'ly' instead. Change when string length should be at least 3.

--- stringsProgram.py
+++ stringsProgram.py
@@ -34,6 +34,20 @@ class programStrings:
         myString = firstChar + myString[1:].replace(firstChar,'$')
         return myString        
 
+    def changeString(self):
+        '''
+        Add 'ing' at the end of a given string (length should be at least 3). 
+        If the given string already ends with 'ing' then add 'ly' instead. 
+        If the string length of the given string is less than 3, leave it unchanged.
+        '''
+        string = input('Enter the String : ')
+        if len(string) > 2:
+            if string[-3:] == 'ing':
+                string += 'ly'
+            else:
+                string += 'ing'
+        return string
+
 def menu():
     '''
     Menu of programs
@@ -42,6 +56,7 @@ def menu():
     1.Calculate the length of a string
     2.Character frequency in a string
     3.Occurrences changed with $
+    4.Add 'ing' or 'ly' at the end 
     ''')
 
 def switchToFunction(case, obj):
@@ -51,7 +66,8 @@ def switchToFunction(case, obj):
     switcher = {
         1 : lambda: obj.lenOfStrings(),
         2 : lambda: obj.charFrequency(),
-        3 : lambda: obj.changeOccurChar()
+        3 : lambda: obj.changeOccurChar(),
+        4 : lambda: obj.changeString()
         }
     func = switcher.get(case, lambda: 'Invalid choice please select correct options.')
     print(func())
