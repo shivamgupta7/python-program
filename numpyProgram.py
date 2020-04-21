fab74ffe7374559a2ec0849ae64ff75c5973505d
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-21 16:59:41 +0530

    [Shivam] Create a null vector of size 10 and update sixth value to 11.

--- numpyProgram.py
+++ numpyProgram.py
@@ -20,6 +20,19 @@ class programNumPy:
         print(matrix)
         return matrix
 
+    def create_update(self):
+        '''
+        Create a null vector of size 10 and update sixth value to 11.
+        '''
+        size = int(input('Enter the null vector of size: '))
+        vector = np.zeros(size)
+        print("Null Vector : ", vector)
+        index = int(input('Enter the position where you want to update: '))
+        value = int(input('Enter the value: '))
+        vector[index-1] = value 
+        print("Update {} position value to {} : ".format(index,value), vector)
+        return vector
+
 def switchToFunction(obj):
     '''
     Create switch function to move perticular program
@@ -27,14 +40,19 @@ def switchToFunction(obj):
     print('''
     1.Convert a list of numeric value into a one-dimensional NumPy array
     2.3x3 matrix with values ranging from 2 to 10
+    3.Create a null vector of size 10 and update sixth value to 11
     ''')
-    choice = int(input('Enter which program you want to run: '))
-    switcher = {
-        1 : obj.listToArray,
-        2 : obj.matrix3x3,
-        }
-    func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
-    func()
+    try:
+        choice = int(input('Enter which program you want to run: '))
+        switcher = {
+            1 : obj.listToArray,
+            2 : obj.matrix3x3,
+            3 : obj.create_update,
+            }
+        func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
+        func()
+    except Exception as e:
+        print("\n",e)
 
 def main():
     obj = programNumPy()
