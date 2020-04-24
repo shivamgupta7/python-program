64dbb943a522c6830f1943ffbb8f9b61e6180c36
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-24 19:06:12 +0530

    [Shivam] Draw a scatter plot comparing two subject marks of Mathematics and Science. Use marks of 10 students.

--- Python-Matplotlib/matplotlibScatter.py
+++ Python-Matplotlib/matplotlibScatter.py
@@ -48,6 +48,23 @@ class programScatterplot:
         plt.savefig('data/4_2.diff_size_ball.png')
         plt.show()
 
+    def compare_two_list(self):
+        '''
+        Draw a scatter plot comparing two subject marks of Mathematics and Science. 
+        Use marks of 10 students.
+        '''
+        math_marks = [88, 92, 80, 89, 100, 80, 60, 100, 80, 34]
+        science_marks = [35, 79, 79, 48, 100, 88, 32, 45, 20, 30]
+        marks_range = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        plt.scatter(marks_range, math_marks, label='Math marks', color='r')
+        plt.scatter(marks_range, science_marks, label='Science marks', color='g')
+        plt.title('Scatter Plot')
+        plt.xlabel('Marks Range')
+        plt.ylabel('Marks Scored')
+        plt.legend()
+        plt.savefig('data/4_3.compare_two_list.png')
+        plt.show()
+
 def switchToFunction(obj):
     '''
     Create switch function to move perticular program
@@ -56,6 +73,7 @@ def switchToFunction(obj):
     1.Scatter graph taking a random distribution in X and Y
     2.Scatter plot with empty circles taking a random distribution in X and Y
     3.Scatter plot using random distributions to generate balls of different sizes
+    4.Draw a scatter plot comparing two list
     ''')
     try:
         choice = int(input('Enter which program you want to run: '))
@@ -63,6 +81,7 @@ def switchToFunction(obj):
             1 : obj.scatter_plot,
             2 : obj.scatter_plot_empty_circle,
             3 : obj.diff_size_ball,
+            4 : obj.compare_two_list,
             }
         func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
         func()
