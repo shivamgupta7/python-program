c44a399e832a6781d6065d8864aa58ed69f9d465
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-24 18:25:16 +0530

    [Shivam] scatter plot with empty circles taking a random distribution in X and Y

--- Python-Matplotlib/matplotlibScatter.py
+++ Python-Matplotlib/matplotlibScatter.py
@@ -17,17 +17,33 @@ class programScatterplot:
         plt.savefig('data/4_1.scatter_plot.png')
         plt.show()
 
+    def scatter_plot_empty_circle(self):
+        '''
+        Draw a scatter plot with empty circles taking a random distribution
+        in X and Y and plotted against each other
+        '''
+        x_axis = randn(20)    # random
+        y_axis = randn(20)
+        plt.scatter(x_axis,y_axis, facecolors='none', edgecolors='r')
+        plt.title('Random distribution in X and Y')
+        plt.xlabel("X-axis")
+        plt.ylabel("Y_axis")
+        plt.savefig('data/4_1.scatter_plot.png')
+        plt.show()
+
 def switchToFunction(obj):
     '''
     Create switch function to move perticular program
     '''
     print('''
     1.Scatter graph taking a random distribution in X and Y
+    2.Scatter plot with empty circles taking a random distribution in X and Y
     ''')
     try:
         choice = int(input('Enter which program you want to run: '))
         switcher = {
             1 : obj.scatter_plot,
+            2 : obj.scatter_plot_empty_circle,
             }
         func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
         func()
