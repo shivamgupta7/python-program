6ddd2a00d7978fe5d775a63019ba01b460955c1f
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-24 18:55:46 +0530

    [Shivam] Scatter plot using random distributions to generate balls of different sizes.

--- Python-Matplotlib/matplotlibScatter.py
+++ Python-Matplotlib/matplotlibScatter.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from pylab import randn
+import random
+import math
 
 class programScatterplot:
 
@@ -31,6 +33,21 @@ class programScatterplot:
         plt.savefig('data/4_1.scatter_plot.png')
         plt.show()
 
+    def diff_size_ball(self):
+        '''
+        Draw a scatter plot using random distributions to generate balls of
+        different sizes.
+        '''
+        x_axis = [random.random() for _ in range(10)]
+        y_axis = [random.random() for _ in range(10)]
+        colors = [random.randint(1, 10) for i in range(10)]
+        areas = [math.pi * random.randint(5, 15)**2 for i in range(10)]
+        plt.scatter(x_axis, y_axis, s = areas, c = colors)
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.savefig('data/4_2.diff_size_ball.png')
+        plt.show()
+
 def switchToFunction(obj):
     '''
     Create switch function to move perticular program
@@ -38,12 +55,14 @@ def switchToFunction(obj):
     print('''
     1.Scatter graph taking a random distribution in X and Y
     2.Scatter plot with empty circles taking a random distribution in X and Y
+    3.Scatter plot using random distributions to generate balls of different sizes
     ''')
     try:
         choice = int(input('Enter which program you want to run: '))
         switcher = {
             1 : obj.scatter_plot,
             2 : obj.scatter_plot_empty_circle,
+            3 : obj.diff_size_ball,
             }
         func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
         func()
