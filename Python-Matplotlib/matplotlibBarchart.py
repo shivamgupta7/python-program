dae7008addf0b6f785afcc853d77bcec5bb5f80b
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-24 14:56:30 +0530

    [Shivam] Display a bar chart use different color for each bar.

--- Python-Matplotlib/matplotlibBarchart.py
+++ Python-Matplotlib/matplotlibBarchart.py
@@ -44,6 +44,21 @@ class programBarchart:
         plt.savefig('data/2_3.uniform_color_bar_chart.png')
         plt.show()
 
+    def diff_color_bar(self):
+        '''
+        Display a bar chart of the popularity of programming Languages. 
+        Use different color for each bar.
+        '''
+        languages = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+        popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+        plt.bar(languages,popularity, color=['r','g','b','y','b','c'])    # color = (r,g,b,transparency)
+        plt.xlabel("Languages")
+        plt.ylabel("Popularity")
+        plt.title("Popularity of Programming Language")
+        plt.grid()
+        plt.savefig('data/2_4.diff_color_bar_chart.png')
+        plt.show()
+
 def switchToFunction(obj):
     '''
     Create switch function to move perticular program
@@ -52,6 +67,7 @@ def switchToFunction(obj):
     1.Display a bar chart of the popularity of programming Languages
     2.Display a horizontal bar chart of the popularity of programming Languages
     3.Display a bar chart of the popularity of programming Languages. Use uniform color.
+    4.Display a bar chart use different color for each bar.
     ''')
     try:
         choice = int(input('Enter which program you want to run: '))
@@ -59,6 +75,7 @@ def switchToFunction(obj):
             1 : obj.bar_chart,
             2 : obj.horizontal_bar_chart,
             3 : obj.uniform_color_bar,
+            4 : obj.diff_color_bar,
             }
         func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
         func()
