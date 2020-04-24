494e427fb4308e50779c8d8d70d0c7c2cdbdc33d
Shivam Gupta <shivamg.cr7@gmail.com>
2020-04-24 17:46:25 +0530

    [Shivam] Read the data from a csv file and create pie chart.

--- Python-Matplotlib/matplotlibPie.py
+++ Python-Matplotlib/matplotlibPie.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class programPiechart:
 
@@ -14,17 +15,31 @@ class programPiechart:
         plt.savefig('data/3_1.pie_chart.png')
         plt.show()
 
+    def pieChartUsingCSV(self):
+        '''
+        create a pie chart of gold medal achievements of five most successful 
+        countries in 2016 Summer Olympics. Read the data from a csv file.
+        '''
+        df = pd.read_csv('data/medal.csv')
+        colors = ["r", "g", "y", "b", "m"]
+        plt.pie(df['gold_medal'],labels=df['country'], colors=colors, autopct='%1.1f%%')
+        plt.title("Five most successful countries in 2016 Summer Olympics")
+        plt.savefig('data/3_2.gold_medal.png')
+        plt.show()
+
 def switchToFunction(obj):
     '''
     Create switch function to move perticular program
     '''
     print('''
     1.Create a pie chart of the popularity of programming Languages
+    2.Read the data from a csv file and create pie chart.
     ''')
     try:
         choice = int(input('Enter which program you want to run: '))
         switcher = {
             1 : obj.pie_chart,
+            2 : obj.pieChartUsingCSV
             }
         func = switcher.get(choice, lambda: print('\nInvalid choice please select correct options.'))
         func()
