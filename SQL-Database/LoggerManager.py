7ca7922bd2271b3d6fc064d82b36bf914fb5d23f
Shivam Gupta <shivamg.cr7@gmail.com>
2020-05-04 20:12:09 +0530

    [Shivam] Modify filemode in logging.config() function.

--- SQL-Database/LoggerManager.py
+++ SQL-Database/LoggerManager.py
@@ -6,5 +6,5 @@ class NewLogger:
 	def __init__(self, source,log_name):
 		self.source =source
 		logger = logging.getLogger(source)
-		logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', filename=log_name, filemode='w', level=logging.DEBUG)
+		logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', filename=log_name, filemode='a', level=logging.DEBUG)
 		logger.info( ' : Starting new task: ')
