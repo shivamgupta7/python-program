c322c077c54cbd03cec037ad66bfda04910d698e
Shivam Gupta <shivamg.cr7@gmail.com>
2020-05-12 12:39:54 +0530

    [Shivam] Adding two new function for export and import data for csv and json file.

--- SQL-Database/DatabaseManager.py
+++ SQL-Database/DatabaseManager.py
@@ -11,7 +11,7 @@ import csv
 class DatabaseManager:
     
     def __init__(self, host_name, username, password, db, logger_file_name=''):
-        self.conn = mysql.connector.connect(host=host_name, user=username, passwd=password, database=db)
+        self.conn = mysql.connector.connect(host=host_name, user=username, passwd=password, database=db, allow_local_infile=True)
         self.database = db
         self.logger_file_name = logger_file_name
         self.database_logger = NewLogger('DatabaseManager', self.logger_file_name)
@@ -79,17 +79,17 @@ class DatabaseManager:
                 self.conn.close()
                 print("MySQL connection is closed")
 
-    def export_data(self,SqlString,file_type):
+    def export_data(self,SqlString,file_type,table_name):
         try:
             if file_type.casefold() == 'json':
-                logging.info('Export database ' + self.database + ' to ' + file_type +' file.')
+                logging.info('Export table ' + table_name + ' to ' + file_type +' file.')
                 self.cursor.execute(SqlString)
                 row_headers = [column[0] for column in self.cursor.description]
                 data=self.cursor.fetchall()
-                json_data = {'Employees': []}
+                json_data = { table_name : []}
                 out_file = open("employees.json", "w")
                 for result in data:
-                    json_data['Employees'].append(dict(zip(row_headers,result)))
+                    json_data[table_name].append(dict(zip(row_headers,result)))
                 json.dump(json_data, out_file,indent=4, default=str)
                 out_file.close()
                 print('Table convert into json file successfully.')
@@ -106,6 +106,20 @@ class DatabaseManager:
                 print('Table convert into CSV file successfully.')
         except mysql.connector.Error as ex:
             logging.error('Export sql data run on ' + self.database + ' for query: '+str(ex))
+        finally:
+            if (self.conn.is_connected()):
+                self.cursor.close()
+                self.conn.close()
+                print("MySQL connection is closed")
+
+    def import_data(self,SqlString):
+        try:
+            logging.info('Import data on ' + self.database + ' database')
+            self.cursor.execute(SqlString)
+            self.conn.commit()
+            print('Import data successfully.')
+        except mysql.connector.Error as ex:
+            logging.error('Import data run on ' + self.database + ' for query: '+str(ex))
         finally:
             if (self.conn.is_connected()):
                 self.cursor.close()
